cad919e2a08ac9c340c6ef2399dc9b5bea2b6b67
Rafal Gwizdala <rafal@cogit.pl>
2018-10-09 08:28:27 +0200

    save

--- src/main/java/lafar6502/App.java
+++ src/main/java/lafar6502/App.java
@@ -18,13 +18,35 @@ import java.io.IOException;
  */
 public class App 
 {
+    private static String inputFile;
+    private static String outputFile;
+    private static String xmlFile;
+    
+    private static void processCmdline(String[] args) throws Exception {
+        for(int i=1; i<args.length; i+= 2) {
+            switch(args[i]) {
+                case "-input":
+                    inputFile = args[i+1];
+                    break;
+                case "-output":
+                    outputFile = args[i+1];
+                    break;
+                case "-xmlfile":
+                    xmlFile = args[i+1];
+                    break;
+                default:
+                    throw new Exception("Unknown switch: " + args[i]);
+            }
+        }
+    }
+    
     public static void main( String[] args ) throws Exception
     {
         if (args.length == 0) {
             String[] usages = {
                 "USAGE",
                 "extractform -input [input pdf file] -output [out file]",
-                "fillform -input [input pdf file] -formfile [input form file]"
+                "fillform -input [input pdf file] -xmlfile [input form data file] -output [output file]"
             };
             for (int i=0; i<usages.length; i++) {
                 System.out.println(usages[i]);
@@ -32,15 +54,32 @@ public class App
             return;
         }
         String cmd = args[0];
+        processCmdline(args);
         switch(cmd) {
             case "extractform":
+                if (inputFile == null) throw new Exception("Missing -input");
+                if (outputFile == null) throw new Exception("Missing -output");
+                extractFormData(inputFile, outputFile);
             break;
             case "fillform":
+                if (inputFile == null) throw new Exception("Missing -input option");
+                if (xmlFile == null) throw new Exception("Missing -xmlfile option");
+                if (outputFile == null) throw new Exception("Missing -output option");
+                updateFormData(inputFile, xmlFile, outputFile);
             break;
             default:
                 throw new Exception("Invalid command: " + cmd);
         }
     }
     
+    private static void extractFormData(String inputFile, String outputFile) throws Exception {
+        ReadXFA xf = new ReadXFA();
+        xf.readXml(inputFile, outputFile);
+    }
+    
+    private static void updateFormData(String inf, String xmlf, String outF) throws Exception {
+        FillXFA2 xf = new FillXFA2();
+        xf.manipulatePdf(inf, xmlf, outF);
+    }
     
 }
